f38e81fd6a770e7eed723c7af7fdc1752460e9f3
Mohammed Hassan <medo.hassan0100@gmail.com>
2022-09-05 16:45:12 +0200

    edit

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -7,9 +7,9 @@
                 <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                     <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3 row">
                         <div class="col-6">
-                            <h5 class="text-white text-capitalize ps-3" style="margin-right: 10px">إضافة قسم جديد</h5>
+                            <h5 class="text-white text-capitalize ps-3" style="margin-right: 10px">تعديل القسم</h5>
                         </div>
-                        <div class="col-6" style="position: relative;"><a href="{{ route('admin.products') }}"
+                        <div class="col-6" style="position: relative;"><a href="{{ route('admin.subs') }}"
                                 style="position: absolute; left: 2%" class="btn btn-primary">عرض الأقسام</a></div>
                     </div>
                 </div>
@@ -24,8 +24,8 @@
                                             <div class="card shadow-2-strong card-registration"
                                                 style="border-radius: 15px;">
                                                 <div class="card-body p-4 p-md-5">
-                                                    <form action="{{ route('admin.product.update', $product->id) }}" method="POST"
-                                                        enctype="multipart/form-data">
+                                                    <form action="{{ route('admin.sub.update', $sub->id) }}"
+                                                        method="POST" enctype="multipart/form-data">
                                                         @csrf
 
                                                         <div class="row">
@@ -34,87 +34,11 @@
                                                                     <label class="form-label" for="title"
                                                                         style="font-size: 18px">الإسم</label>
                                                                     <input type="text" name="title" id="title"
-                                                                        class="form-control form-control-lg formborderCSS" required value="{{ $product->title }}" />
+                                                                        class="form-control form-control-lg formborderCSS" value="{{ $sub->title }}" />
                                                                 </div>
+
                                                             </div>
                                                         </div>
-
-                                                        <div class="row">
-                                                            <div class="col-md-12 mb-4">
-                                                                <div class="form-outline">
-                                                                    <label class="form-label" for="description"
-                                                                        style="font-size: 18px">الوصف</label>
-                                                                    <textarea name="description" id="description" cols="30" rows="5"
-                                                                    class="form-control form-control-lg formborderCSS" required>{{ $product->description }}</textarea>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-
-                                                        <div class="row">
-                                                            <div class="col-md-12 mb-4">
-                                                                <div class="form-outline">
-                                                                    <label class="form-label" for="amount"
-                                                                        style="font-size: 18px">الكمية</label>
-                                                                    <input type="text" name="amount" id="amount"
-                                                                        class="form-control form-control-lg formborderCSS" required value="{{ $product->amount }}"/>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-
-                                                        <div class="row">
-                                                            <div class="col-md-6 mb-4">
-                                                                <div class="form-outline">
-                                                                    <label class="form-label" for="old_price"
-                                                                        style="font-size: 18px">السعر القديم</label>
-                                                                    <input type="number" name="old_price" id="old_price" step=".01"
-                                                                        class="form-control form-control-lg formborderCSS" required value="{{ $product->old_price }}" />
-                                                                </div>
-                                                            </div>
-                                                            <div class="col-md-6 mb-4">
-                                                                <div class="form-outline">
-                                                                    <label class="form-label" for="new_price"
-                                                                        style="font-size: 18px">السعر الجديد</label>
-                                                                    <input type="number" name="new_price" id="new_price" step=".01"
-                                                                        class="form-control form-control-lg formborderCSS" required value="{{ $product->new_price }}" />
-                                                                </div>
-                                                            </div>
-                                                        </div>
-
-                                                        <div class="row">
-                                                            <div class="col-md-12 mb-4">
-                                                                <div class="form-outline">
-                                                                    <label class="form-label" for="image"
-                                                                        style="font-size: 18px">الصورة</label>
-                                                                    <div class="row">
-                                                                        <div class="col-6">
-                                                                            <img src="{{ asset($product->image) }}" class="img-thumbnail" alt="product_photo">
-                                                                        </div>
-                                                                        <div class="col-6">
-                                                                            <input type="file" name="image" id="image"
-                                                                        class="form-control form-control-lg formborderCSS" />
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-
-
-                                                        <div class="row">
-                                                            <div class="col-12">
-                                                                <div class="form-group mx-sm-3 mb-2">
-                                                                    <label class="form-label" for="sub_id"
-                                                                        style="font-size: 18px">اختيار قسم - شركة</label>
-                                                                    <select name="sub_id" id="sub_id" class="form-control form-control-lg formborderCSS"
-                                                                        class="form-control" required>
-                                                                        @foreach ($subs as $sub)
-                                                                            <option value="{{ $sub->id }}" {{isset($product) ? $product->sub_id == $sub->id ? 'selected' : '' : old('sub_id')}}>
-                                                                                {{ $sub->title }} --> {{ $sub->categories->title }}</option>
-                                                                        @endforeach
-                                                                    </select>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-
 
                                                         <div class="mt-4 pt-2 text-center">
                                                             <input class="btn btn-primary btn-lg" type="submit"
